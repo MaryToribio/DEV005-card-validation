4a22a894f03d2cd088f28fc06ccb42f2c17bc814
Maria <mari3030florencia@gmail.com>
2023-02-13 12:02:24 -0500

    Estrucctura js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import validator from './validator.js';
+function numeroDeTarjeta() {
+    let numero = document.getElementById("numeroDeTarjetaDeCredito").value;
+    alert(numero);
+    algoritmoDeLuhn(numero)
+}
 
-console.log(validator);
+function algoritmoDeLuhn(numeroDeTarjeta) {
+    let numeroDeTarjetaArray = Array.from(numeroDeTarjeta)
+    let numeroDeTarjetaArrayInvertido = numeroDeTarjetaArray.reverse()
+    for (let index = 0; index < numeroDeTarjetaArrayInvertido.length; index++) {
+        let indiceNormal = index + 1;
+
+    }
+
+
+}
