799bf511466c2c7e2f07ec2857700358bcc6b0f4
Monica <monica.toribio.rojas@gmail.com>
2023-02-18 21:29:50 -0500

    avance

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-function numeroDeTarjeta() {
-    let numero = document.getElementById("numeroDeTarjetaDeCredito").value;
-    alert(numero);
-    algoritmoDeLuhn(numero)
+document.getElementById('botonValidar').addEventListener('click', function(e) {
+        e.preventDefault()
+        console.log('Quiero ir por chicharron de pescado');
+    }) 
+
+document.getElementById('botonValidar').addEventListener('click', validarNumeros)
+
+function validarNumeros(e) {
+    e.preventDefault()
+    const numeroIngresados = document.getElementById("numeroTcIngresado").value;
+    // alert(numeroIngresados);
+    console.log(sumarNumeros(numeroIngresados));
 }
 
-function algoritmoDeLuhn(numeroDeTarjeta) {
-    let numeroDeTarjetaArray = Array.from(numeroDeTarjeta)
-    let numeroDeTarjetaArrayInvertido = numeroDeTarjetaArray.reverse()
-    for (let index = 0; index < numeroDeTarjetaArrayInvertido.length; index++) {
-        let indiceNormal = index + 1;
+function sumarNumeros(todoslosnumeros) {
+    let numerosTransformados = parseInt(todoslosnumeros)
+    const crerSuma = numerosTransformados + numerosTransformados
+    return crerSuma
+}
+
+function algoritmoDeLuhn() {
+
+    const numeroIngresadoString = numeroIngresado.toString();
+    const numIngStringArray = Array.from(numeroIngresadoString);
+    const numIngStringArrayInv = numIngStringArray.reverse();
+
+    for (let index = 0; index < numIngStringArrayInv.length; index++) {
+        const indiceNormal = index + 1;
 
     }
 
