f0bd4a6953cf675a3c96c638efd1c584f1e6ea1d
Monica <monica.toribio.rojas@gmail.com>
2023-02-20 10:55:15 -0500

    se agrego el algoritmo de luhn en js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
-document.getElementById('botonValidar').addEventListener('click', function(e) {
-        e.preventDefault()
-        console.log('Quiero ir por chicharron de pescado');
-    }) 
+import validator from './validator.js';
 
-document.getElementById('botonValidar').addEventListener('click', validarNumeros)
-
-function validarNumeros(e) {
+document.getElementById("botonValidar").addEventListener("click", function(e) {
     e.preventDefault()
-    const numeroIngresados = document.getElementById("numeroTcIngresado").value;
-    // alert(numeroIngresados);
-    console.log(sumarNumeros(numeroIngresados));
-}
-
-function sumarNumeros(todoslosnumeros) {
-    let numerosTransformados = parseInt(todoslosnumeros)
-    const crerSuma = numerosTransformados + numerosTransformados
-    return crerSuma
-}
-
-function algoritmoDeLuhn() {
-
-    const numeroIngresadoString = numeroIngresado.toString();
-    const numIngStringArray = Array.from(numeroIngresadoString);
-    const numIngStringArrayInv = numIngStringArray.reverse();
-
-    for (let index = 0; index < numIngStringArrayInv.length; index++) {
-        const indiceNormal = index + 1;
-
+    const numeroIngresado = document.getElementById("numeroTcIngresado").value
+    const resultadoValidacion = validator.esUnaTarjetaValida(numeroIngresado)
+    if(resultadoValidacion){
+        alert("correcto")
+    }else{
+        alert("incorrecto")
     }
-
-
-}
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,68 @@
+// creo un objeto 
 const validator = {
-  // ...
+  esUnaTarjetaValida : (numeroTarjeta) => {
+    if(numeroTarjeta.length == 16){
+      let arrayNumerico = invertirTarjeta(numeroTarjeta);
+      arrayNumerico = multiplicarPares(arrayNumerico);
+      const total = sumarElementosArray(arrayNumerico);
+      if(total % 10 == 0){
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false
+    }
+  }
 };
+
+
+function invertirTarjeta(numeroTarjeta) {
+  const arrayTarjeta = Array.from(numeroTarjeta).reverse();
+  const arrayNumerico = arrayTarjeta.map((numeroString) => Number(numeroString));
+  return arrayNumerico;  
+}
+
+ 
+function multiplicarPares(arrayNumerico) {
+  for (let indice = 0; indice < arrayNumerico.length; indice++) {
+    const indiceReal = indice + 1
+    if (esPar(indiceReal)) {
+      arrayNumerico[indice] = arrayNumerico[indice] * 2;
+
+      if(esNumeroDosDigitos( arrayNumerico[indice] )){
+        arrayNumerico[indice] = sumarDigitos(arrayNumerico[indice]);
+      }
+    }
+  }
+  return arrayNumerico;
+}
+
+function esPar(numero){
+  return (numero % 2 ) == 0;
+}
+
+function esNumeroDosDigitos(numero){
+  return numero >9;
+}
+
+function sumarDigitos(numero) {
+  const numeroString = numero.toString();
+  const arrayNumero = Array.from(numeroString);
+  
+  let acumulador = 0;
+  arrayNumero.forEach((element) => {
+      acumulador += Number(element);
+  });
+  return acumulador;
+}
+
+function sumarElementosArray(arrayNumerico){
+  let total = 0;
+  arrayNumerico.forEach((element) => {
+    total += element;
+  });
+  return total
+}
 
 export default validator;
